eae9d11b155a9e2b1c45fe4a2989891b84da71bc
Jakub Kujawski <jaahcob@gmail.com>
2024-08-23 16:45:01 +0100

    Add files via upload
    This is my first Python code, few examples of making calculations, int, float, and printing strings, \n - new line, print(r"") - r at the beginning creates Raw line which ignore \n new line commands

---
+++ Python-1_23:08:24.py
@@ -0,0 +1,72 @@
+Python 3.12.5 (v3.12.5:ff3bc82f7c9, Aug  7 2024, 05:32:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+1+2
+3
+1-2
+-1
+3*2
+6
+3**2
+9
+3 ** 3
+27
+5 / 2
+2.5
+5 // 2
+2
+8 + 9 - 10
+7
+8 + 2 * 3
+14
+(8 + 2) * 3
+30
+10 // 3
+3
+10 % 3
+1
+'Jakub'
+'Jakub'
+print ('Jakub')
+Jakub
+print("jakub")
+jakub
+print("Jakub's Laptop")
+Jakub's Laptop
+>>> 
+>>> print("Jakub's "laptop"")
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print("Jakub's"+'"laptop"')
+Jakub's"laptop"
+>>> print("Jakub's "+'"laptop"')
+Jakub's "laptop"
+>>> 
+>>> print('Jakub\'s "laptop"')
+Jakub's "laptop"
+>>> 
+>>> 10 * "Jakub "
+'Jakub Jakub Jakub Jakub Jakub Jakub Jakub Jakub Jakub Jakub '
+>>> 
+>>> 
+>>> print("c:\docs\nJakub")
+c:\docs
+Jakub
+>>> 
+>>> print(r"c:docs\nJakub")
+c:docs\nJakub
+>>> // comment?
+SyntaxError: invalid syntax
+>>> \\Jakub
+SyntaxError: unexpected character after line continuation character
+>>> # this is comment
+>>> print("Jakub")
+Jakub
+>>> # print(r"jakub/n") - r raw String
+>>> # \n - new line
+>>> 
+>>> print("Jakub\nJakub\nJakub")
+Jakub
+Jakub
+Jakub
+>>> 
+>>> print(r"Jakub\nJakub\nJakub\n")
+Jakub\nJakub\nJakub\n
